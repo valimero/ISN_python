b047598f6ea28fb3b7d1a1379b55de518fc48216
Valimero <valimero.ilou@gmail.com>
2015-12-02 18:07:01 +0100

    work

---
+++ TP03/ex09.py
@@ -0,0 +1,7 @@
+#ex09
+#Renvoie l'identifiant de l'utilisateur
+
+prenom = input("Quel est votre prenom ?\n")
+nom = input("\nQuel est votre nom ?\n")
+
+print ('\n' + "Voici votre identifiant: " + prenom.lower()[0] + nom.lower()[0:9])
