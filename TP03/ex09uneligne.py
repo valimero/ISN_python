a181eef9ceb661f3a46d7b0a6b6014106aa14d04
Valimero <valimero.ilou@gmail.com>
2015-12-02 18:22:04 +0100

    bonus

---
+++ TP03/ex09uneligne.py
@@ -0,0 +1,4 @@
+#ex09
+#Renvoie l'identifiant de l'utilisateur
+
+print ('\n' + "Voici votre identifiant: " + input("Quel est votre prenom ?\n").lower()[0] + input("\nQuel est votre nom ?\n").lower()[0:9])
