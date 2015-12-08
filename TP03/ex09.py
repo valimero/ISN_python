b913e2d35375df5fff65589dc131b96422b95a08
Valimero <valimero.ilou@gmail.com>
2015-12-08 20:04:37 +0100

    patch

--- TP03/ex09.py
+++ TP03/ex09.py
@@ -4,4 +4,4 @@
 prenom = input("Quel est votre prenom ?\n")
 nom = input("\nQuel est votre nom ?\n")
 
-print ('\n' + "Voici votre identifiant: " + prenom.lower()[0] + nom.lower()[0:9])
+print ('\n' + "Voici votre identifiant: " + prenom.lower()[0] + nom.lower()[0:8])
