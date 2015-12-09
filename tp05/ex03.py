471576a30e4a95453c6c7f23b8b9df660a575318
Valimero <valimero.ilou@gmail.com>
2015-12-09 19:21:01 +0100

    add comment

--- tp05/ex03.py
+++ tp05/ex03.py
@@ -1,12 +1,14 @@
 from time import time, sleep
 
-deb = time()
+deb = time()		# Stocke l'instant t au lancement du programme
 
 i = 1
 while (i < 6):
 	print(i)
-	sleep(1)
+	if i != 5: 		# Pour ne pas avoir de pause après l'affichage du dernier caractère
+		sleep(1)
 	i = i + 1
 
-end = time()
+end = time()		# Stocke l'instant t a la fin du programme pour permettre le calcul du temps d'éxécution.
+
 print("Ce programme s'est execute en " + str(end - deb) + " seconde(s)")
